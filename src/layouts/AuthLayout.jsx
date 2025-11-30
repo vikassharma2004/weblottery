3f5ceac2776b4b75644a69144c0cec90d6aea9f8
vikasFixl <vikasfixl@gmail.com>
2025-11-30 12:00:36 +0530

    fixed the lazy load of images

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,11 @@
-export default function AuthLayout({ children,src }) {
+export default function AuthLayout({ children, src }) {
   return (
     <div className="min-h-screen flex">
       {/* --- Left Side (Image Section) --- */}
       <div className="hidden lg:flex w-1/2 bg-gray-100 items-center justify-center p-10">
         <img
-      src={src}
-
+          src={src}
+          loading="lazy"
           alt="Auth Illustration"
           className="max-w-[70%] rounded-2xl shadow-lg object-cover"
         />
@@ -13,9 +13,7 @@ export default function AuthLayout({ children,src }) {
 
       {/* --- Right Side (Form Section) --- */}
       <div className="w-full lg:w-1/2 flex items-center justify-center bg-white px-6 py-12">
-        <div className="w-full max-w-md">
-          {children}
-        </div>
+        <div className="w-full max-w-md">{children}</div>
       </div>
     </div>
   );
